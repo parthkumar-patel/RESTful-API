8e180e3f6088de514113382fdcba4a6bcaf09115
Parth <parthp15@yahoo.com>
2024-04-11 13:21:00 -0700

    basic api made

---
+++ index.js
@@ -0,0 +1,14 @@
+const app = require('express')();
+const PORT = 8080;
+
+app.listen(
+    PORT,
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
+
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        tshirt: '👕',
+        size: 'large'
+    })
+});
